6ac57584caf867c340529f51f6a1e70b140b2aaa
kevinfiero <kjf46@cornell.edu>
2020-10-28 14:20:17 -0700

    statically add one pokemon

--- src/PokeItem.js
+++ src/PokeItem.js
@@ -3,24 +3,26 @@ import React, { Component } from 'react'
 export default class PokeItem extends Component {
     render() {
         return (
-            <section>
+            <section className = 'column center border' style={{backgroundColor: "purple"}}>
                 <h2>Bulbasaur</h2>
                 <img src = 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png' />
-                <ul> Stats:
-                    <li>HP: 45</li>
-                    <li>Attack: 49</li>
-                    <li>Defense: 49</li>
-                    <li>Speed: 45</li>
-                </ul>
-                <ul> Type(s):
-                    <li>Grass</li>
-                    <li>Poison</li>
-                </ul>
-                <ul> Shape:
-                    <li>Quadruped</li>
-                </ul>
-
-
+                <div className = 'row'>
+                    <ul> Stats:
+                        <li>HP: 45</li>
+                        <li>Attack: 49</li>
+                        <li>Defense: 49</li>
+                        <li>Speed: 45</li>
+                    </ul>
+                    <div className = 'column'>
+                        <ul> Type(s):
+                            <li>Grass</li>
+                            <li>Poison</li>
+                        </ul>
+                        <ul> Shape:
+                            <li>Quadruped</li>
+                        </ul>
+                    </div>
+                </div>
             </section>
         )
     }
