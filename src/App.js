d964acf4e7ab88f0e83abc19b9692bb738519e5d
kevinfiero <kjf46@cornell.edu>
2020-10-28 18:46:41 -0700

    styling changes

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ export default class App extends Component {
     return (
       <>
         <Header />
-        <div className = 'row'>
+        <div className = 'row background-tan'>
           <Searchbar sort2Change = {this.sort2Change} sort1Change = {this.sort1Change} buttonClick = {this.buttonClick} textChange = {this.textChange} />
           <PokeList sort2 = {this.state.sort2} sort1 = {this.state.sort1} pokemonData = {pokemonData} filter = {this.state.filter} />
         </div>
--- src/Searchbar.js
+++ src/Searchbar.js
@@ -3,20 +3,31 @@ import React, { Component } from 'react'
 export default class SearchBar extends Component {
     render() {
         return (
-            <div id= 'search-bar' className = 'border center column'>
-                Search by Name:
-               <input type='text' onChange = {this.props.textChange}/>
-               <button onClick = {this.props.buttonClick}>Go!</button>
-               <select onChange = {this.props.sort1Change}>
-                   <option value ='ascending'>Ascending</option>
-                   <option value ='descending'>Descending</option>
-               </select>
-               <select onChange = {this.props.sort2Change}>
-                    <option value ='pokemon'>Pokemon</option>
-                    <option value ='type_1'>Primary Type</option>
-                    <option value ='attack'>Attack</option>
-                    <option value ='defense'>Defense</option>
-               </select>
+            <div id= 'search-bar' className = 'border center column even'>
+                <div id='box' className = 'border'>
+                    <label>Search By Name:</label>
+                    <input type='text' onChange = {this.props.textChange}/>
+                    <button onClick = {this.props.buttonClick}>Go!</button>
+                </div>
+               <div id='drop' className = 'border'>
+                    <span>
+                    <label>Sort by </label>
+                    <select onChange = {this.props.sort2Change}>
+                        <option value ='pokemon'>Name</option>
+                        <option value ='type_1'>Primary Type</option>
+                        <option value ='attack'>Attack</option>
+                        <option value ='defense'>Defense</option>
+                    </select>
+                    <label> in</label>
+                    </span>
+                    <select onChange = {this.props.sort1Change}>
+                        <option value ='ascending'>Ascending</option>
+                        <option value ='descending'>Descending</option>
+                    </select>
+                    <label>order</label>
+               </div>
+
+               
             </div>
         )
     }
